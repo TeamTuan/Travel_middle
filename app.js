987ef4f708cce2e6a1bc4fc0692d0cb15795e53b
你看我名字帅气么 <676962537@qq.com>
2017-06-07 23:56:31 +0800

    转发请求

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(cors({
 }));
 
 
+
+
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/first', function(req, res, next) {
-  
-  res.json({name:'aaa',pwd:'123'});
+  var result=[];
+  request.get('http://127.0.0.1/Travel_hou/blog/index', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      result=body;
+      res.json(result);
+    }
+  });
 });
+
+router.get('/login', function(req, res, next) {
+  var tel=req.query.tel;
+  var password=req.query.password;
+  request.get('http://127.0.0.1/Travel_hou/user/index?tel='+tel+"&password="+password,function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  });
+});
+
+router.get('/user_by_id', function(req, res, next) {
+  var id=req.query.id;
+  request.get('http://127.0.0.1/Travel_hou/user/select_user_by_id?id='+id,function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  });
+});
+
+
+
 
 
 
