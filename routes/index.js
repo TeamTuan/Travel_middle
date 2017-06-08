8655e14078e783181796f9eedc6d8bb17174c51b
你看我名字帅气么 <676962537@qq.com>
2017-06-08 11:34:22 +0800

    转发请求

--- routes/index.js
+++ routes/index.js
@@ -44,11 +44,13 @@ router.get('/check_login', function(req, res, next) {
 });
 
 router.get('/get_blog', function(req, res, next) {
+
   request.get('http://127.0.0.1/Travel_hou/blog/index', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.json(body);
     }
   });
+
 });
 
 
